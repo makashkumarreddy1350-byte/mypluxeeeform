0594e0a5dce9ad8dc149fefb02f22c0c5853c8c7
makashkumarreddy1350-byte <makashkumarreddy1350@gmail.com>
2026-05-31 17:17:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if (username === "1234" && password === "1234") {
+    if (username === "pluxee@form" && password === "pluxee@1350") {
 
         window.location.href = "form.html";
 
